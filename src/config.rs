64c99a094972207250e0015b4203946feac7b731
Yaroslav <94973178+Sagruvi@users.noreply.github.com>
2023-04-22 08:40:44 +0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -5,10 +5,10 @@ pub struct server_config{
     pub api_url: String,
     pub host: u32,
     pub port: u32,
-}
+} 
 pub fn get_config() -> server_config {
     dotenv().ok();
-    let api_url = env::var("sk-...kxmv").unwrap();
+    let api_url = env::var("API-KEY").unwrap();
     let host = env::var("HOST").unwrap().parse::<u32>().unwrap();
     let port = env::var("PORT").unwrap().parse::<u32>().unwrap();
     server_config {
